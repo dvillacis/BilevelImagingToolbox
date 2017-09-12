4245931ab81677d1da0225d7ca5159401102f721
David Villacis <david@villacis.net>
2017-09-12 17:38:12 -0400

    Added cuda files for ROF Chambolle-Pock

---
+++ bilevel_imaging_toolbox/cuda/rof_chambolle_pock_dual_step.cu
@@ -0,0 +1,28 @@
+__global__ void primal(float* p1, float* p2, const float* u_, 
+		                       const double lambda, const double sigma,
+				                              const int X, const int Y)
+{
+	int x = blockIdx.x*blockDim.x + threadIdx.x;
+	int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+	// center point
+	int c = y*X + x;
+
+	float nabla_x = 0.0f;
+	float nabla_y = 0.0f;
+
+	if (x < X-1)
+		nabla_x = u_[c+1]-u_[c];
+
+	if (y < Y-1)
+		nabla_y = u_[c+X]-u_[c];
+
+		        //p1[c] = fmaxf(-lambda, fminf(lambda, p1[c] + sigma*nabla_x));
+		        //p2[c] = fmaxf(-lambda, fminf(lambda, p2[c] + sigma*nabla_y));
+
+	p1[c] += sigma*nabla_x;
+	p2[c] += sigma*nabla_y;
+	float denom = fmaxf(1.0f, sqrt(p1[c]*p1[c] + p2[c]*p2[c])/lambda);
+	p1[c] /= denom;
+	p2[c] /= denom;
+}
---
+++ bilevel_imaging_toolbox/cuda/rof_chambolle_pock_primal_step.cu
@@ -0,0 +1,32 @@
+__global__ void primal(float* u, float* u_, const float* f, const float* p1,
+		                       const float* p2, const double tau, const double theta, 
+				                              const int X, const int Y)
+{
+	int x = blockIdx.x*blockDim.x + threadIdx.x;
+	int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+	// center point
+	int c = y*X + x;
+
+	float div_x = 0.0f;
+	float div_y = 0.0f;
+
+	if (x == 0)
+		div_x = p1[c];
+	if (x > 0 & x < X-1)
+		div_x = p1[c]-p1[c-1];
+	if (x == X-1)
+		div_x = -p1[c-1];
+			  
+	if (y == 0)
+		div_y = p2[c];
+	if (y > 0 && y < Y-1)
+		div_y = p2[c]-p2[c-X];
+	if (y == Y-1)
+		div_y = -p2[c-X];
+			        
+	float u_old = u[c];
+	u[c] = (u_old + tau*(+div_x+div_y+f[c]))/(1+tau);
+	u_[c] = (1+theta)*u[c]-theta*u_old;
+}
+
