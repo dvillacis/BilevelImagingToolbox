22a593ee3ea707c84f01a0a71fbadf6959070286
David Villacis <david@villacis.net>
2017-09-14 15:55:30 -0500

    added cpu testing

---
+++ tests/test_synthetic_cpu.py
@@ -0,0 +1,64 @@
+from bilevel_imaging_toolbox import solvers
+from bilevel_imaging_toolbox import image_utils
+import timeit
+
+s_image_list = ['../examples/images/circle.png','../examples/images/cameraman.png','../examples/images/lena.png']
+n_trials = 3
+n_iters = 100
+
+print('Testing FB ROF CPU')
+for image_path in s_image_list:
+    times = np.zeros(3)
+    image = image_utils.load_image(image_path)
+    image = image_utils.convert_to_grayscale(image)
+    g_image = image_utils.add_gaussian_noise(image)
+
+    # Parameter Definition
+    clambda = 0.2
+    sigma = 1.9
+    tau = 0.9/sigma
+
+    for k in range(n_trials):
+        start_time = timeit.default_timer()
+        (fb_image,fb_values) = solvers.forward_backward_ROF(g_image,clambda,tau,iters=n_iters)
+        times[k] = timeit.default_timer()-start_time
+
+    print('Execution time for FB for ROF on '+image_path+': %d,%d'%(np.mean(times),np.std(times)))
+
+print('Testing CP ROF CPU')
+for image_path in s_image_list:
+    times = np.zeros(3)
+    image = image_utils.load_image(image_path)
+    image = image_utils.convert_to_grayscale(image)
+    g_image = image_utils.add_gaussian_noise(image)
+
+    # Parameter Definition
+    clambda = 0.2
+    sigma = 1.9
+    tau = 0.9/sigma
+
+    for k in range(n_trials):
+        start_time = timeit.default_timer()
+        (fb_image,fb_values) = solvers.chambolle_pock_ROF(g_image,clambda,tau,sigma,iters=n_iters)
+        times[k] = timeit.default_timer()-start_time
+
+    print('Execution time for CP for ROF on '+image_path+': %d,%d'%(np.mean(times),np.std(times)))
+
+print('Testing CP TVl1 CPU')
+for image_path in s_image_list:
+    times = np.zeros(3)
+    image = image_utils.load_image(image_path)
+    image = image_utils.convert_to_grayscale(image)
+    g_image = image_utils.add_gaussian_noise(image)
+
+    # Parameter Definition
+    clambda = 0.2
+    sigma = 1.9
+    tau = 0.9/sigma
+
+    for k in range(n_trials):
+        start_time = timeit.default_timer()
+        (fb_image,fb_values) = solvers.chambolle_pock_TVl1(g_image,clambda,tau,sigma,iters=n_iters)
+        times[k] = timeit.default_timer()-start_time
+
+    print('Execution time for CP for ROF on '+image_path+': %d,%d'%(np.mean(times),np.std(times)))
